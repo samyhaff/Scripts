553347078f9f4f8fb0bcd299b833d528b97cd1ed
Samy Haffoudhi <haffoudhisamy@gmail.com>
2021-10-23 19:28:24 +0200

    updates

---
+++ convertWall.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+from ImageGoNord import GoNord
+
+parser = argparse.ArgumentParser(description='convert image palette to specified palette')
+parser.add_argument('colorscheme', type=str, help='name of the colorscheme')
+parser.add_argument('image', type=str, help='path to the image')
+args = parser.parse_args()
+
+path = "/home/samy/.config/colorschemes/" + args.colorscheme
+
+with open(path, 'r') as f:
+    lines = f.readlines()
+
+colors = [line.split()[2] for line in lines]
+
+go_nord = GoNord()
+go_nord.reset_palette()
+for color in colors:
+    go_nord.add_color_to_palette(color)
+
+print(args.image)
+image = go_nord.open_image(args.image)
+go_nord.convert_image(image, save_path='/home/samy/Wallpapers/wall.png')
